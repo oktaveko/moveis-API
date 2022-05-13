2f457824d5dff4cddc9941a30eca4ea5e5fe4b6a
oktaveko <fransokta77@gmail.com>
2022-05-13 21:14:02 +0700

    add put & delete genres test

--- routes/genres.js
+++ routes/genres.js
@@ -1,10 +1,10 @@
 const validateObjectId = require('../middleware/validateObjectId');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
-const express = require('express');
-const mongoose = require('mongoose');
-const router = express.Router();
 const {Genre, validate} = require('../models/genre');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
 
 
 
@@ -24,7 +24,7 @@ router.post('/', auth, async (req,res) => {
     res.send(genre);
 });
 
-router.put('/:id',auth, async (req,res) => {
+router.put('/:id',[auth, validateObjectId], async (req,res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
     
@@ -38,7 +38,7 @@ router.put('/:id',auth, async (req,res) => {
     res.send(genre);
 });
 
-router.delete('/:id', [auth, admin], async(req,res) => {
+router.delete('/:id', [auth, admin, validateObjectId], async(req,res) => {
     const genre = await Genre.findByIdAndRemove(req.params.id);
 
     // const genre = genres.find(c => c.id === parseInt(req.params.id));
